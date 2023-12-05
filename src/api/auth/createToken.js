21573a0a6e9139fe9e18489cc2ffc463ae5aef40
Gabriel Cabral Melo <contatogabemelo@gmail.com>
2023-12-05 16:46:28 -0300

    adiciona método de criação de token

---
+++ src/api/auth/createToken.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.JWT_SECRET;
+const jwtConfig = { expiresIn: '7d', algorithm: 'HS256' };
+
+const createToken = (tokenData) => {
+  const token = jwt.sign(tokenData, secret, jwtConfig);
+
+  return token;
+};
+
+module.exports = createToken;
